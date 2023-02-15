c8f72ca9ed82a8e5d08b7848adc4080b34a47fe7
Duke-Skygawker <bart3kk990@gmail.com>
2023-02-15 15:05:29 +0100

    implemented Apollo in Products

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,6 +1,7 @@
-import styled from 'styled-components';
-import SubHeader from '../../components/SubHeader';
-import ProductItem from '../../components/ProductItem';
+import styled from "styled-components";
+import { useQuery, gql } from "@apollo/client";
+import SubHeader from "../../components/SubHeader";
+import ProductItem from "../../components/ProductItem";
 
 const ProductItemsWrapper = styled.div`
   display: flex;
@@ -10,12 +11,33 @@ const ProductItemsWrapper = styled.div`
   margin: 2% 5%;
 `;
 
+const GET_PRODUCTS = gql`
+  query getProducts {
+    products {
+      id
+      title
+      price
+      thumbnail
+    }
+  }
+`;
+
 function Products() {
+  const { loading, data } = useQuery(GET_PRODUCTS);
   return (
     <>
-      <SubHeader title='Available products' goToCart />
-
-      <ProductItemsWrapper></ProductItemsWrapper>
+      <SubHeader title="Available products" goToCart />
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <ProductItemsWrapper>
+          {data &&
+            data.products &&
+            data.products.map((product) => (
+              <ProductItem key={product.id} data={product} />
+            ))}
+        </ProductItemsWrapper>
+      )}
     </>
   );
 }
