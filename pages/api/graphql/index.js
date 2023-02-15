cc6b217caf1245c8b16a42b0be71215235d15315
Duke-Skygawker <bart3kk990@gmail.com>
2023-02-15 12:39:04 +0100

    expanded GQL shcema, added Apollo

--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -8,15 +8,30 @@ const typeDefs = /* GraphQL */ `
     title: String!
     thumbnail: String!
     price: Float
+    category: Category
+  }
+  type Category {
+    id: Int!
+    title: String!
   }
   type Query {
     product: Product
     products(limit: Int): [Product]
+    categories: [Category]
   }
 `;
 
+const mocks = {
+  Int: () => Math.floor(Math.random() * 99) + 1,
+  Float: () => (Math.random() * 99.0 + 1.0).toFixed(2),
+  Product: () => ({
+    thumbnail: () => "https://picsum.photos/400/400",
+  }),
+};
+
 const executableSchema = addMocksToSchema({
   schema: makeExecutableSchema({ typeDefs }),
+  mocks,
 });
 
 function runMiddleware(req, res, fn) {
