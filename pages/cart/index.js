f40ede8d8c1897e087db40cb28b3eb6b29dc7f37
Duke-Skygawker <bart3kk990@gmail.com>
2023-02-17 19:08:05 +0100

    cart authentication

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
+import { useRouter } from "next/router";
 import SubHeader from "../../components/SubHeader";
 import ProductItem from "../../components/ProductItem";
 import Button from "../../components/Button";
@@ -32,8 +33,18 @@ const GET_CART = gql`
   }
 `;
 
+const COMPLETE_CART = gql`
+  mutation completeCart {
+    completeCart {
+      complete
+    }
+  }
+`;
+
 function Cart() {
   const { loading, data } = useQuery(GET_CART);
+  const [completeCard] = useMutation(COMPLETE_CART);
+  const route = useRouter();
 
   return (
     <>
@@ -49,9 +60,24 @@ function Cart() {
                 <ProductItem key={product.id} data={product} />
               ))}
           </CartItemsWrapper>
-          {data && data.cart.products.length > 0 && (
-            <Button backgroundColor="royalBlue">Checkout</Button>
-          )}
+          {data &&
+            data.cart.products.length > 0 &&
+            sessionStorage.getItem("token") && (
+              <Button
+                backgroundColor="royalBlue"
+                onClick={() => {
+                  const isAuthenticated = sessionStorage.getItem("token");
+
+                  if (isAuthenticated) {
+                    completeCard();
+                    alert("Thanks! Cart is emptied");
+                    route.push("/");
+                  }
+                }}
+              >
+                Checkout
+              </Button>
+            )}
         </CartWrapper>
       )}
     </>
